4e994a4008e340b2f17ebc0c5281d3cd3cdad780
harshitnitjsr <harshit26092004@gmail.com>
2024-03-04 13:15:56 +0530

    minor fix

--- Social/backend/server.js
+++ Social/backend/server.js
@@ -82,19 +82,19 @@ io.on("connection", (socket) => {
 })
 // --------------------------deployment------------------------------
 
-const __dirname1 = path.resolve();
+// const __dirname1 = path.resolve();
 
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname1, "/FrontEnd/build")));
+// if (process.env.NODE_ENV === "production") {
+//   app.use(express.static(path.join(__dirname1, "/FrontEnd/build")));
 
-  app.get("*", (req, res) =>
-    res.sendFile(path.resolve(__dirname1, "FrontEnd", "build", "index.html"))
-  );
-} else {
+//   app.get("*", (req, res) =>
+//     res.sendFile(path.resolve(__dirname1, "FrontEnd", "build", "index.html"))
+//   );
+// } else {
   app.get("/", (req, res) => {
     res.send("API is running..");
   });
-}
+// }
 
 // --------------------------deployment------------------------------
 
